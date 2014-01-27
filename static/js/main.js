ebad4ce8c32579c70c1402943133557d6ae68630
Helen Lin <helen51796@gmail.com>
2014-01-27 16:43:57 -0500

    change

--- static/js/main.js
+++ static/js/main.js
@@ -3,10 +3,10 @@
 $(document).ready(function(){
   $("button").click(function(){
     $("div").animate({left:'250px'});
-    $("div").animate({left:'250px'});
-    $("div").animate({left:'250px'});
-    $("div").animate({left:'250px'});
-    $("div").animate({left:'250px'});
+    $("div").animate({down:'30px'});
+    $("div").animate({up:'30px'});
+    $("div").animate({down:'30px'});
+    $("div").animate({up:'30px'});
     $("div").animate({right:'250px'});
   });
 });
