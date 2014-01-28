2b342d1cce29065234e1d7fdedcf35d9fb352e37
Jason <jzhen46@gmail.com>
2014-01-28 08:33:00 -0500

    Attempt at interactive pet

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,13 @@
+$(document).nousemove(function (e) {
+    $("#image").stop().animate( {
+	left: e.pageX,
+	top. e.pageY
+    });
+});
 
+
+
+/*
 // code that is supposed to make the pet jump after clicking a button
 $(document).ready(function(){
   $("button").click(function(){
@@ -14,3 +23,4 @@ $(document).ready(function(){
 $(img).mousemove(function(e){
       $('.follow').css({'top': e.clientY - 20, 'left': e.clientX - 20});
 })
+*/
