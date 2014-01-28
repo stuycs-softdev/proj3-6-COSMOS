29a39fbcd5c87db74f6278eea131406a57911a4e
julie-zhu <zhu.julie2@gmail.com>
2014-01-28 01:44:06 -0500

    Update auth.py

--- auth.py
+++ auth.py
@@ -18,3 +18,23 @@ def authenticate(user, pword):
         return True
     else:
         return False
+
+def getName(pname):  
+    
+    
+    
+def getHealth(pname):
+    list = db.info.find_one( {"name":pname}, fields = {"_id_":False})
+    return list["health"]
+    
+def getHunger(pname):
+    list = db.info.find_one( {"name":pname}, fields = {"_id_":False})
+    return list["hunger"]
+    
+def getClean(pname):
+    list = db.info.find_one( {"name":pname}, fields = {"_id_":False})
+    return list["cleanliness"]
+
+def getHappiness(pname):
+    list = db.info.find_one( {"name":pname}, fields = {"_id_":False})
+    return list["happiness"]
